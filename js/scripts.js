e17a4cb193fdc8ff6cbe92ce29697f4cb99db35b
davidlutta <davidlutta2010@gmail.com>
2018-03-27 06:41:40 +0300

    Added JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ function openNav(){
 function closeNav(){
   document.getElementById("mynav").style.width= "0%"
 }
+$(document).ready(function(){
+  $("#blank form").submit(function(event){
+    var email = ["email"];
+    email.forEach(function(blank){
+      var userInput = $("input#" + email).val();
+      alert("Welcome aboard"$("." + email).text(userInput));
+    });
+    event.preventDefault();
+  });
+});
